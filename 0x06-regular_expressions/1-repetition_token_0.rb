4646bf0f7731081d75ae323cacd1d27231a98d18
poquitodev <122402632+poquitodev@users.noreply.github.com>
2023-08-27 16:32:24 +0100

    Delete 0x06-regular_expressions directory

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/hbttn/).join
-puts ARGV[0].scan(/hbtttn/).join
-puts ARGV[0].scan(/hbttttn/).join
-puts ARGV[0].scan(/hbtttttn/).join
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/htn/).join
-puts ARGV[0].scan(/hbtn/).join
