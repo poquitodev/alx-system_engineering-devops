da2aaf1045b1049360e4746a57bf7734a1a2417c
poquitodev <122402632+poquitodev@users.noreply.github.com>
2023-09-04 02:37:16 +0100

    Delete 0x06-regular_expressions directory

--- 0x06-regular_expressions/0-simply_match_school.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-# This script accepts one argument and pass it to a regular expression matching method
-
-puts ARGV[0].scan(/school/i).join
--- 0x06-regular_expressions/1-repetition_token_0.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-# This script accepts one argument and pass it to a regular expression matching method
-
-# puts ARGV[0].scan(/hbtt{1,4}n/).join
-# puts ARGV[0].scan(/hbt{2,5}n/).join
-puts ARGV[0].scan(/^hbt{2,5}n/).join
