8e2e423367c4bc39b6cd9513421f9e2a8172278b
poquitodev <122402632+poquitodev@users.noreply.github.com>
2024-01-18 00:55:39 +0100

    let see

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+0-subs.py queries to https://www.reddit.com/dev/api/
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    for a subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    """
+    returns the number of subscribers
+    """
+    domain = 'https://www.reddit.com'
+    path = '/r/{}/about.json'.format(subreddit)
+    url = '{}{}'.format(domain, path)
+    header = {
+        'user-agent': 'one-dope-boy',
+        'over18': 'yes'
+    }
+    response = requests.get(
+        url,
+        headers=header,
+        allow_redirects=False
+    )
+    code = response.status_code
+    if code >= 300:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
+    data = response.json().get('data')
+    subscribers = data.get('subscribers')
+    return subscribers
